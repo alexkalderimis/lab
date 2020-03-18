96427fe524737cf72e71c7fc005f2711242e1aa0
Alex Kalderimis <alex.kalderimis@gmail.com>
2020-03-18 14:44:42 +0000

    Add useful issue list tools

--- cmd/issue_list.go
+++ cmd/issue_list.go
@@ -7,15 +7,28 @@ import (
 	"github.com/spf13/cobra"
 	gitlab "github.com/xanzy/go-gitlab"
 	lab "github.com/zaquestion/lab/internal/gitlab"
+	strings "strings"
 )
 
 var (
-	issueLabels []string
-	issueState  string
-	issueSearch string
-	issueNumRet int
-	issueAll    bool
+	issueLabels           []string
+	issueShowScopedLabels []string
+	issueState            string
+	issueSearch           string
+	issueNumRet           int
+	issueAll              bool
+	issueMine             bool
+	issueAssignee         string
+	issueAuthor           string
+	issueAssigneeID       *int
+	issueAuthorID         *int
 )
+
+type PrintableIssue struct {
+	iid    string
+	labels string
+	title  string
+}
 
 var issueListCmd = &cobra.Command{
 	Use:     "list [remote] [search]",
@@ -31,14 +44,27 @@ lab issue list remote "search terms"  # search "remote" for issues with "search 
 		if err != nil {
 			log.Fatal(err)
 		}
+		if issueMine {
+			issueAssignee = "@"
+		}
+		err = getUserId(issueAssignee, &issueAssigneeID)
+		if err != nil {
+			log.Fatal(err)
+		}
+		err = getUserId(mrAuthor, &issueAuthorID)
+		if err != nil {
+			log.Fatal(err)
+		}
 
 		opts := gitlab.ListProjectIssuesOptions{
 			ListOptions: gitlab.ListOptions{
 				PerPage: issueNumRet,
 			},
-			Labels:  issueLabels,
-			State:   &issueState,
-			OrderBy: gitlab.String("updated_at"),
+			Labels:     issueLabels,
+			State:      &issueState,
+			OrderBy:    gitlab.String("updated_at"),
+			AssigneeID: issueAssigneeID,
+			AuthorID:   issueAuthorID,
 		}
 
 		if issueSearch != "" {
@@ -53,27 +79,73 @@ lab issue list remote "search terms"  # search "remote" for issues with "search 
 		if err != nil {
 			log.Fatal(err)
 		}
-		for _, issue := range issues {
-			fmt.Printf("#%d %s\n", issue.IID, issue.Title)
+		var w strings.Builder
+		var printables []PrintableIssue
+		printables = make([]PrintableIssue, len(issues), len(issues))
+		for i, issue := range issues {
+			w.Reset()
+			written := 0
+			for _, scope := range issueShowScopedLabels {
+				for _, label := range issue.Labels {
+					if strings.HasPrefix(label, scope) {
+						if written > 0 {
+							w.WriteString(",")
+						}
+						w.WriteString("[" + label + "]")
+						written += 1
+					}
+				}
+			}
+			printables[i] = PrintableIssue{
+				iid:    fmt.Sprintf("#%d", issue.IID),
+				labels: w.String(),
+				title:  issue.Title,
+			}
+		}
+		maxIIDLen := 0
+		maxLabelLen := 0
+		for _, p := range printables {
+			if len(p.iid) > maxIIDLen {
+				maxIIDLen = len(p.iid)
+			}
+			if len(p.labels) > maxLabelLen {
+				maxLabelLen = len(p.labels)
+			}
+		}
+		for _, p := range printables {
+			fmt.Printf("%*s %*s %s\n", -maxIIDLen, p.iid, -maxLabelLen, p.labels, p.title)
 		}
 	},
 }
 
 func init() {
-	issueListCmd.Flags().StringSliceVarP(
+	cmd := issueListCmd
+	cmd.Flags().StringSliceVarP(
 		&issueLabels, "label", "l", []string{},
 		"Filter issues by label")
-	issueListCmd.Flags().StringVarP(
+	cmd.Flags().StringSliceVarP(
+		&issueShowScopedLabels, "show-label", "", []string{},
+		"Show labels with a given scope (e.g. 'workflow' for 'workflow::x' and 'workflow::y')")
+	cmd.Flags().StringVarP(
 		&issueState, "state", "s", "opened",
 		"Filter issues by state (opened/closed)")
-	issueListCmd.Flags().IntVarP(
+	cmd.Flags().IntVarP(
 		&issueNumRet, "number", "n", 10,
 		"Number of issues to return")
-	issueListCmd.Flags().BoolVarP(
+	cmd.Flags().BoolVarP(
 		&issueAll, "all", "a", false,
 		"List all issues on the project")
+	cmd.Flags().BoolVarP(
+		&issueMine, "mine", "m", false,
+		"List only issues assigned to me")
+	cmd.Flags().StringVar(
+		&issueAssignee, "assignee", "",
+		"List only issues assigned to $username (or @ for assigned to me)")
+	cmd.Flags().StringVar(
+		&issueAuthor, "author", "",
+		"List only MRs authored by $username (or @ for by me)")
 
-	issueListCmd.MarkZshCompPositionalArgumentCustom(1, "__lab_completion_remote")
-	issueListCmd.MarkFlagCustom("state", "(opened closed)")
-	issueCmd.AddCommand(issueListCmd)
+	cmd.MarkZshCompPositionalArgumentCustom(1, "__lab_completion_remote")
+	cmd.MarkFlagCustom("state", "(opened closed)")
+	issueCmd.AddCommand(cmd)
 }
